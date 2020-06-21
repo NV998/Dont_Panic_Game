c5d3f1673d48a644d36d76b63a9fdf78fe233914
NV998 <67225865+NV998@users.noreply.github.com>
2020-06-21 16:44:38 +0200

    Dont Panic Game

---
+++ Dont_Panic/Assets/Scripts/LichtSwitch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LichtSwitch : MonoBehaviour {
+
+	public GameObject lichtje1;
+	public GameObject lichtje2;
+	bool knopje;
+
+	void Update(){
+		if(knopje){
+			lichtje1.SetActive (false);
+			lichtje2.SetActive (false);
+		}
+		else{
+			lichtje1.SetActive (true);
+			lichtje2.SetActive (true);
+		}
+
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			knopje = !knopje;
+		}
+	}
+}
